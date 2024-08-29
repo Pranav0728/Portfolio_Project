cf06142b83cdea0f8431416031d66a8629a15a80
FameILY <bhambidsatyam@gmail.com>
2024-08-29 21:17:01 +0530

    book file api added

--- src/app/api/book/route.js
+++ src/app/api/book/route.js
@@ -2,7 +2,24 @@ import { NextResponse } from "next/server";
 import Book from "@/models/Book";
 import connectMongoDB from "@/lib/db";
 import { isApiValid } from "@/lib/function";
+import path from "path";
+import { promises as fs } from 'fs';
+import { writeFile } from "fs/promises";
 
+
+async function generateUniqueFileName(directory, fileName) {
+  let counter = 1;
+  let newFileName = fileName;
+  const fileExtension = path.extname(fileName);
+  const fileNameWithoutExtension = path.basename(fileName, fileExtension);
+
+  while (await fs.stat(path.join(directory, newFileName)).catch(() => false)) {
+    newFileName = `${fileNameWithoutExtension}(${counter})${fileExtension}`;
+    counter += 1;
+  }
+  
+  return newFileName;
+}
 
 export async function GET(req) {
   try {
@@ -32,8 +49,28 @@ export async function POST(req) {
     if (!isApiValid(apiKey)) {
       return NextResponse.json("Unauthorized", { status: 401 });
     }
+
+    const formData = await req.formData();
+
+    // Get the file and store it first
+    const file = formData.get('file');
+    if (!file) {
+      console.log("No files received")
+    }
+    
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const fileName = file.name.replaceAll(" ", "_");
+    const fullPathDirectory = path.join(process.cwd(), "public/assets/books");
+    const uniqueFileName = await generateUniqueFileName(fullPathDirectory, fileName);
+    const imagePath = "/assets/books/" + uniqueFileName;
+
+    await writeFile(
+      path.join(fullPathDirectory, uniqueFileName),
+      buffer
+    );
+
     await connectMongoDB();
-    const data = await req.json();
+    const data = await formData
     const {
       author,
       publisher,
@@ -53,6 +90,7 @@ export async function POST(req) {
         referred,
         isbn,
         level,
+        imagePath
     });
     return NextResponse.json(
       { message: "Book Added Successfully" },
