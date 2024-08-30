13c51f8de388d2fa2d27a804f688e3cf1c81107e
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2024-08-30 11:29:18 +0800

    minor changes2

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,13 +3,13 @@ import Header from "../components/sidebar/sidebar";
 
 export default  function loading(){
     return(
-        <div>
+        <main className="abounew">
             <Header />
-           <div className="h-screen w-full flex justify-center items-center">
-            <div className="flex justify-center items-center h-full">
+           <section className="h-screen aboutnew-content w-[120%] flex justify-center items-center">
+            <div className="flex timelinenew justify-center items-center h-full">
             <div className="w-10 h-10 border-2 border-blue-600 rounded-full loader"></div>
             </div>
-            </div>
-        </div>
+            </section>
+        </main>
     );
 }
