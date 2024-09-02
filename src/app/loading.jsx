7720027ed4a072467b39f543f068716a769a6dc9
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2024-09-02 17:24:06 +0800

    loader

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import Header from "../components/sidebar/sidebar";
 
-export default  function loading(){
-    return(
-        <main className="abounew">
-            <Header />
-           <section className="h-screen aboutnew-content w-[120%] flex justify-center items-center">
-            <div className="flex timelinenew justify-center items-center h-full">
-            <div className="w-10 h-10 border-2 border-blue-600 rounded-full loader"></div>
-            </div>
-            </section>
-        </main>
-    );
+export default function Loading() {
+  return (
+    <main className="aboutnew">
+      <Header />
+      <section className="h-[120vh] mt-[-5rem] aboutnew-content w-[90%] flex justify-center items-center">
+        <div className="flex timelinenew justify-center items-center h-full">
+          <div className="loader"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
